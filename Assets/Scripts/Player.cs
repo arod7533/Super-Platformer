4ff97c754a426779fcce9d05107d287bc3e6979b
Tony Rodriguez <arod7533@gmail.com>
2020-12-08 14:57:49 -0500

    added enemy, dying, and hazard features

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour {
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(25f, 25f);
 
     //State
     bool isAlive = true;
@@ -37,10 +38,14 @@ public class Player : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if(!isAlive){
+            return;
+        }
         Run(); 
         Jump();
         FlipSprite();
         ClimbLadder();
+        Die();
     }
 
     private void ClimbLadder(){
@@ -90,6 +95,14 @@ public class Player : MonoBehaviour {
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
         if(playerHasHorizontalSpeed){
             transform.localScale = new Vector2 (Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+
+    private void Die(){
+        if(myBodyCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards"))){
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
         }
     }
 
